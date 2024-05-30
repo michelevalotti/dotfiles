5a47878b4b676a2fce3cd3dbde0c11b840bd40f9
michelevalotti <michele.valotti@gmail.com>
2024-05-30 17:50:42 +0100

    add regex to ts parsers

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup({
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "bash", "markdown", "markdown_inline" },
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "bash", "markdown", "markdown_inline", "regex" },
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = { "python" }
