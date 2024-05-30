36541d6a04fac2c3379320596b1343891cc4c441
michelevalotti <michele.valotti@gmail.com>
2024-05-30 17:50:08 +0100

    add noice

---
+++ nvim/lua/plugins/noice.lua
@@ -0,0 +1,33 @@
+local noice = require('noice')
+noice.setup({
+  presets = {
+    long_message_to_split = true, -- long messages will be sent to a split
+  },
+  lsp = {
+    hover = { enabled = false },
+    signature = { enabled = false },
+    progress = { enabled = false },
+    message = { enabled = false },
+    smart_move = { enabled = false },
+  },
+})
+
+vim.keymap.set("c", "<C-e>", function()
+  noice.redirect(vim.fn.getcmdline())
+end, { desc = "Redirect Cmdline" })
+
+require("lualine").setup({
+  sections = {
+    lualine_x = {
+      {
+        noice.api.statusline.mode.get,
+        cond = noice.api.statusline.mode.has,
+        color = { fg = "#ff9e64" },
+      },
+      'encoding',
+      'fileformat',
+      'filetype',
+    },
+  },
+})
+require("telescope").load_extension("noice")
