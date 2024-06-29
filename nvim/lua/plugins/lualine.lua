f0551828877979135c7d1ecdbba7ddd8586d27a0
michelevalotti <michele.valotti@gmail.com>
2024-06-29 15:47:34 +0100

    bubble theme

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -12,11 +12,15 @@ end
 require('lualine').setup({
   options = {
     theme = "catppuccin",
-    disabled_filetypes = {'starter'}
+    disabled_filetypes = {'starter'},
+    component_separators = '￨',
+    section_separators = { left = '', right = '' },
   },
   sections = {
+    lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
     lualine_b = { 'branch', {'diff', source = diff_source}, 'diagnostics' },
     lualine_c = { '%f%m' },
+    lualine_z = { { 'location', separator = { right = '' }, left_padding = 2 } },
   },
   extensions = { 'toggleterm' },
 })
